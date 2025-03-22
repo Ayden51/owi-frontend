c54ee5a3129f5d9da2af25738b66efac4fe698f7
Ayden51 <dconghuy0@gmail.com>
2025-03-22 14:52:54 +0700

    feat(ui): Add shadcn-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,19 @@ export default defineNuxtConfig({
         "@nuxt/fonts",
         "@nuxt/image",
         "@nuxt/test-utils",
+        "@nuxtjs/tailwindcss",
+        "shadcn-nuxt",
     ],
+
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: "",
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: "./components/ui",
+    },
 });
