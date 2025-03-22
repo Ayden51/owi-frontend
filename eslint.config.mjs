3e1bab91eb3cd2f01cad868fe6e68b7cf32cdc71
Ayden51 <dconghuy0@gmail.com>
2025-03-22 14:30:35 +0700

    chore: Update config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
-  // Your custom configs here
-)
+    // Your custom configs here
+    {
+        ignores: [".nuxt", ".nitro", ".output", "build", "node_modules"],
+    }
+);
