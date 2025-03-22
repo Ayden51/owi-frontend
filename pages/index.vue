1206dc2a891fc5ac49bf92bae5822eca52b8cf17
Ayden51 <dconghuy0@gmail.com>
2025-03-22 15:07:08 +0700

    feat: Configure Nuxt basic layout

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="grid place-items-center h-dvh">
+        <Button as-child><a href="/demo">Go to Invitation Demo Page</a></Button>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
