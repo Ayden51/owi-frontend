15801f23d3e80a342db650b8f629227e0776ed74
Ayden51 <dconghuy0@gmail.com>
2025-04-02 09:41:23 +0000

    feat(ui): Add components for web layout

---
+++ components/wedding/shared/WebContainer.vue
@@ -0,0 +1,20 @@
+<template>
+    <component :is="props.as" :class="cn('container max-w-screen-sm mx-auto', props.class)">
+        <slot />
+    </component>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import type { AsTag } from "reka-ui";
+
+interface Props {
+    class?: HTMLAttributes["class"];
+    as?: AsTag | Component;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    class: "",
+    as: "div",
+});
+</script>
