21ddc760ecd908c605269ef7ea391b5c7f6c61e7
Ayden51 <dconghuy0@gmail.com>
2025-04-22 14:16:28 +0000

    refactor(ui): Remove MasonryGallery component, move the gallery into the section file, fix layout shift issue with gallery by providing width and height

--- components/wedding/template-a/GalleryA.vue
+++ components/wedding/template-a/GalleryA.vue
@@ -9,34 +9,30 @@ const props = defineProps<{
 const defaultData: GalleryData = {
     title: "ALBUM ẢNH CƯỚI",
     images: [
-        { src: "/images/gallery/gallery-1.jpg", alt: "Album Photo 1", loading: "lazy" },
-        { src: "/images/gallery/gallery-2.jpg", alt: "Album Photo 2", loading: "lazy" },
-        { src: "/images/gallery/gallery-3.jpg", alt: "Album Photo 3", loading: "lazy" },
-        { src: "/images/gallery/gallery-4.jpg", alt: "Album Photo 4", loading: "lazy" },
-        { src: "/images/gallery/gallery-5.jpg", alt: "Album Photo 5", loading: "lazy" },
-        { src: "/images/gallery/gallery-6.jpg", alt: "Album Photo 6", loading: "lazy" },
-        { src: "/images/gallery/gallery-7.jpg", alt: "Album Photo 7", loading: "lazy" },
-        { src: "/images/gallery/gallery-8.jpg", alt: "Album Photo 8", loading: "lazy" },
-        { src: "/images/gallery/gallery-9.jpg", alt: "Album Photo 9", loading: "lazy" },
-        { src: "/images/gallery/gallery-10.jpg", alt: "Album Photo 10", loading: "lazy" },
-        { src: "/images/gallery/gallery-11.jpg", alt: "Album Photo 11", loading: "lazy" },
-        { src: "/images/gallery/gallery-12.jpg", alt: "Album Photo 12", loading: "lazy" },
-        { src: "/images/gallery/gallery-13.jpg", alt: "Album Photo 13", loading: "lazy" },
-        { src: "/images/gallery/gallery-14.jpg", alt: "Album Photo 14", loading: "lazy" },
-        { src: "/images/gallery/gallery-15.jpg", alt: "Album Photo 15", loading: "lazy" },
-        { src: "/images/gallery/gallery-16.jpg", alt: "Album Photo 16", loading: "lazy" },
-        { src: "/images/gallery/gallery-17.jpg", alt: "Album Photo 17", loading: "lazy" },
-        { src: "/images/gallery/gallery-18.jpg", alt: "Album Photo 18", loading: "lazy" },
-        { src: "/images/gallery/gallery-19.jpg", alt: "Album Photo 19", loading: "lazy" },
-        { src: "/images/gallery/gallery-20.jpg", alt: "Album Photo 20", loading: "lazy" },
-        { src: "/images/gallery/gallery-21.jpg", alt: "Album Photo 21", loading: "lazy" },
-        { src: "/images/gallery/gallery-22.jpg", alt: "Album Photo 22", loading: "lazy" },
-        { src: "/images/gallery/gallery-23.jpg", alt: "Album Photo 23", loading: "lazy" },
-        { src: "/images/gallery/gallery-24.jpg", alt: "Album Photo 24", loading: "lazy" },
-        { src: "/images/gallery/gallery-25.jpg", alt: "Album Photo 25", loading: "lazy" },
-        { src: "/images/gallery/gallery-26.jpg", alt: "Album Photo 26", loading: "lazy" },
-        { src: "/images/gallery/gallery-27.jpg", alt: "Album Photo 27", loading: "lazy" },
-        { src: "/images/gallery/gallery-28.jpg", alt: "Album Photo 28", loading: "lazy" },
+        { src: "/images/gallery/gallery-1.jpg", alt: "Album Photo 1", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-2.jpg", alt: "Album Photo 2", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-3.jpg", alt: "Album Photo 3", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-4.jpg", alt: "Album Photo 4", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-5.jpg", alt: "Album Photo 5", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-6.jpg", alt: "Album Photo 6", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-7.jpg", alt: "Album Photo 7", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-8.jpg", alt: "Album Photo 8", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-9.jpg", alt: "Album Photo 9", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-10.jpg", alt: "Album Photo 10", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-11.jpg", alt: "Album Photo 11", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-12.jpg", alt: "Album Photo 12", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-13.jpg", alt: "Album Photo 13", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-14.jpg", alt: "Album Photo 14", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-15.jpg", alt: "Album Photo 15", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-16.jpg", alt: "Album Photo 16", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-17.jpg", alt: "Album Photo 17", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-18.jpg", alt: "Album Photo 18", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-19.jpg", alt: "Album Photo 19", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-20.jpg", alt: "Album Photo 20", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-21.jpg", alt: "Album Photo 21", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-22.jpg", alt: "Album Photo 22", width: 208, height: 138.667 },
+        { src: "/images/gallery/gallery-23.jpg", alt: "Album Photo 23", width: 208, height: 312 },
+        { src: "/images/gallery/gallery-24.jpg", alt: "Album Photo 24", width: 208, height: 138.667 },
     ],
 };
 
@@ -57,7 +53,16 @@ const data = computed(() => ({
                 {{ data.title }}
             </h2>
 
-            <MasonryGallery :images="data.images" />
+            <div class="columns-2 sm:columns-3 gap-x-2">
+                <NuxtPicture
+                    v-for="(image, imgIndex) in data.images"
+                    :key="imgIndex"
+                    :placeholder="[image.width, image.height, 30, 5]"
+                    :img-attrs="{ ...image.imgAttrs, class: cn('h-auto w-full mb-2', image.imgAttrs?.class ?? '') }"
+                    loading="lazy"
+                    v-bind="removeKeysFromProps(image, 'imgAttrs')"
+                />
+            </div>
         </WebContainer>
     </WebSection>
 </template>
