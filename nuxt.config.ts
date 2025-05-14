e55cff520ba7907d8091d69bdd23332a9c98d5e0
Ayden51 <dconghuy0@gmail.com>
2025-05-14 07:12:12 +0000

    feat: Add @vueuse/motion module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,15 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
 
-    modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image", "@nuxt/test-utils", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+    modules: [
+        "@nuxt/eslint",
+        "@nuxt/fonts",
+        "@nuxt/image",
+        "@nuxt/test-utils",
+        "@nuxtjs/tailwindcss",
+        "shadcn-nuxt",
+        "@vueuse/motion/nuxt",
+    ],
     components: {
         dirs: [
             {
