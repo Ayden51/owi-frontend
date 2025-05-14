3133bcdf37c581e81d7b8ee5a3cfeaef19afbb68
Ayden51 <dconghuy0@gmail.com>
2025-05-14 10:42:16 +0000

    feat: add nuxt-easy-lightbox module to enhance image viewing capabilities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
         "@nuxtjs/tailwindcss",
         "shadcn-nuxt",
         "@vueuse/motion/nuxt",
+        "nuxt-easy-lightbox",
     ],
     components: {
         dirs: [
