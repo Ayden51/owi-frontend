1a39b99bdef2e54f921eab7343308caa62558d6b
Ayden51 <dconghuy0@gmail.com>
2025-06-02 08:40:54 +0000

    feat: update favicon and add web app manifest with new icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,14 @@ export default defineNuxtConfig({
 
     app: {
         head: {
-            link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+            link: [
+                { rel: "icon", type: "image/x-icon", sizes: "48x48", href: "/favicon.ico" },
+                { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+                { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+                { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+                { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+                { rel: "manifest", href: "/site.webmanifest" },
+            ],
         },
     },
 
