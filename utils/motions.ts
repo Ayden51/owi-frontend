d8e7d29be92227ef07e5654eaa4bc130012e9b7e
Ayden51 <dconghuy0@gmail.com>
2025-06-02 08:41:57 +0000

    feat: enhance motion directives with additional fade and pop effects

--- utils/motions.ts
+++ utils/motions.ts
@@ -1,12 +1,41 @@
 import type { Transition, MotionVariants } from "@vueuse/motion";
 
-const defaultTransition: Transition = {
+export const defaultTransition: Transition = {
     duration: 800,
     ease: "easeInOut",
 };
 
 export const motionDirectives = {
+    "w-fade": {
+        initial: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-fade-once": {
+        initial: {
+            opacity: 0,
+        },
+        visibleOnce: {
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
     "w-fade-up": {
+        initial: {
+            y: 100,
+            opacity: 0,
+        },
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-fade-up-once": {
         initial: {
             y: 100,
             opacity: 0,
@@ -22,6 +51,17 @@ export const motionDirectives = {
             y: -100,
             opacity: 0,
         },
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-fade-down-once": {
+        initial: {
+            y: -100,
+            opacity: 0,
+        },
         visibleOnce: {
             y: 0,
             opacity: 1,
@@ -29,6 +69,17 @@ export const motionDirectives = {
         },
     },
     "w-fade-left": {
+        initial: {
+            x: 100,
+            opacity: 0,
+        },
+        visible: {
+            x: 0,
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-fade-left-once": {
         initial: {
             x: 100,
             opacity: 0,
@@ -44,8 +95,41 @@ export const motionDirectives = {
             x: -100,
             opacity: 0,
         },
+        visible: {
+            x: 0,
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-fade-right-once": {
+        initial: {
+            x: -100,
+            opacity: 0,
+        },
         visibleOnce: {
             x: 0,
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-pop": {
+        initial: {
+            scale: 0,
+            opacity: 0,
+        },
+        visible: {
+            scale: 1,
+            opacity: 1,
+            transition: defaultTransition,
+        },
+    },
+    "w-pop-once": {
+        initial: {
+            scale: 0,
+            opacity: 0,
+        },
+        visibleOnce: {
+            scale: 1,
             opacity: 1,
             transition: defaultTransition,
         },
